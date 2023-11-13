c7a4f4d6760dd32255f0d06a0b07324e07791df1
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2023-11-13 11:00:26 +0700

    feat: create icons component

---
+++ src/components/elements/Icons/Icons.tsx
@@ -0,0 +1,35 @@
+import CartIcon from "@/components/icons/CartIcon";
+import UserIcon from "@/components/icons/UserIcon";
+import { MappedSizeVariant, SizeVariant } from "@/types/componentsTypes";
+import { twMerge } from "tailwind-merge";
+
+export type IconsName = "cart" | "user";
+
+const ICON_SIZES: MappedSizeVariant & { "x-small": string } = {
+  "x-small": "h-4 w-4",
+  small: "h-5 w-5",
+  large: "h-6 w-6",
+};
+
+type Props = {
+  name: IconsName;
+  className?: string;
+  size?: SizeVariant | "x-small";
+  width?: number;
+  height?: number;
+};
+
+export default function Icons(props: Props) {
+  const { name, className, size = "small", width, height } = props;
+
+  const classNames = twMerge(ICON_SIZES[size], className, "shrink-0 grow-0");
+
+  switch (name) {
+    case "cart":
+      return <CartIcon className={classNames} />;
+    case "user":
+      return <UserIcon className={classNames} />;
+    default:
+      throw new Error("Invalid icon name");
+  }
+}
