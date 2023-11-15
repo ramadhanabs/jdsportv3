4c1d0fe6fb43fd4efabb14cc6b60d3ccc807b034
Ramadhana Bagus Solichuddin <ramadhanabagus99@gmail.com>
2023-11-15 08:57:57 +0700

    fix: import devicetype

--- src/utils/componentUtils.ts
+++ src/utils/componentUtils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-bitwise */
-import { DeviceType } from "@helpers/getDeviceType";
+import { DeviceType } from "@/helpers/getDeviceType";
 
 type ComponentLocator =
   | "@slider-desktop"
@@ -80,3 +80,20 @@ export const generateBannerList = (data: string) => {
 
   return JSON.stringify(object);
 };
+
+export const toBase64 = (str: string) =>
+  typeof window === "undefined" ? Buffer.from(str).toString("base64") : window.btoa(str);
+
+export const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#DEDEDE" offset="20%" />
+      <stop stop-color="#AAAAAA" offset="50%" />
+      <stop stop-color="#DEDEDE" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#DEDEDE" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
