652930df9848e7cc177a36e94e6a717807109648
Ramadhana Bagus Solichuddin <ramadhanabagus99@gmail.com>
2023-11-15 08:58:34 +0700

    chore: add config for remotePatterns image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME,
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
