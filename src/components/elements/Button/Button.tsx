c9ba87c49397a0f16d8db2a9abd335480979fd0c
Ramadhana Bagus Solichuddin <ramadhanabagus99@gmail.com>
2023-11-17 16:02:32 +0700

    feat: add props isLoading

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -1,14 +1,15 @@
 import React, { ButtonHTMLAttributes, forwardRef, LegacyRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/utils/componentUtils";
+import Spinner from "../Spinner/Spinner";
 
 export const buttonVariants = cva(
-  "text-sm flex items-center justify-center gap-3 font-semibold w-max",
+  "text-sm flex items-center justify-center gap-3 font-semibold w-max disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-white border border-transparent hover:bg-white hover:border-primary transition-all hover:text-black",
+          "bg-primary text-white border border-transparent hover:bg-primary/90 hover:border-primary transition-all hover:text-black",
         secondary: "bg-secondary text-black",
         outline: "border border-black text-black hover:bg-black hover:text-white",
         unstyled: "",
@@ -26,15 +27,22 @@ export const buttonVariants = cva(
 
 export interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
 
 const Button = (
-  { className, children, variant, size, ...props }: ButtonProps,
+  { isLoading, className, children, variant, size, ...props }: ButtonProps,
   ref: LegacyRef<HTMLButtonElement>
 ) => {
   return (
-    <button ref={ref} className={cn(buttonVariants({ variant, size, className }))} {...props}>
-      {children}
+    <button
+      ref={ref}
+      className={cn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? <Spinner /> : children}
     </button>
   );
 };
